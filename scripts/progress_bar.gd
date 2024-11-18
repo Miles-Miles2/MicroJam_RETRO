dbd75fd9b5b05b1ab3916e9c89c1e0f4f33cd3db
Spunboing <diamondjellybean457@gmail.com>
2024-11-17 17:39:28 -0800

    face icon

--- scripts/progress_bar.gd
+++ scripts/progress_bar.gd
@@ -5,6 +5,11 @@ extends ProgressBar
 @onready var gamemanager: Node = $"../../gamemanager"
 @onready var progress_bar: ProgressBar = $"."
 
+@onready var tex1: TextureRect = $"../TextureRect"
+@onready var tex2: TextureRect = $"../TextureRect2"
+@onready var tex3: TextureRect = $"../TextureRect3"
+@onready var tex4: TextureRect = $"../TextureRect4"
+
 func _ready():
 	timer.start()
 	
@@ -14,12 +19,30 @@ func _on_timer_timeout() -> void:
 	if gamemanager.num_of_active_events <= 1:
 		value -= 2
 	else:
-		value += gamemanager.num_of_active_events*.05
-	if 66 < value && value <= 100:
+		value += gamemanager.num_of_active_events*6
+	if 90 < value && value <= 100:
+		set_modulate(Color(.21,.053,0))
+		tex1.visible = false
+		tex2.visible = false
+		tex3.visible = false
+		tex4.visible = true
+	if 66 < value && value <= 90:
 		set_modulate(Color(.784,0,.121))
+		tex1.visible = false
+		tex2.visible = false
+		tex3.visible = true
+		tex4.visible = false
 	if 33 < value && value <= 66:
 		set_modulate(Color(.707,.298,0))
+		tex1.visible = false
+		tex2.visible = true
+		tex3.visible = false
+		tex4.visible = false
 	if value <= 33:
 		set_modulate(Color(.761,.769,0))
+		tex1.visible = true
+		tex2.visible = false
+		tex3.visible = false
+		tex4.visible = false
 
 	timer.start()
